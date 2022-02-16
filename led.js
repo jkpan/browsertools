29d0a60b868c20fa74cac5597b633e581c4e16f2
jkpan <jkuangpan@gmail.com>
2022-02-16 17:22:25 +0800

    0216

--- led.js
+++ led.js
@@ -519,9 +519,9 @@ function gray(coloramount) {
 
 function reduceColor(coloramount) {
   let block = Math.ceil(255/coloramount);
-  _r = Math.floor(_r/block) * block * 2/3;
-  _g = Math.floor(_g/block) * block * 2/3;
-  _b = Math.floor(_b/block) * block * 2/3;
+  _r = Math.floor(_r/block) * block;
+  _g = Math.floor(_g/block) * block;
+  _b = Math.floor(_b/block) * block;
 }
 
 function contrast(coloramount) {
