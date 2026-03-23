53b86cd855afb40c439f579df65f9933443ba366
jkpan <jkuangpan@gmail.com>
2026-03-23 09:42:03 +0800

    0323

--- subtitle/sub_b_voice.js
+++ subtitle/sub_b_voice.js
@@ -97,17 +97,48 @@ function initRecognition() {
 
   recognition = new webkitSpeechRecognition();
   recognition.continuous = true;
+  //recognition.interimResults = true;
+  recognition.lang = "zh-TW";//'cmn-Hant-TW';//'zh-TW';//'en-US';'en-US';'cmn-Hant-TW';
   recognition.interimResults = true;
-  recognition.lang = 'cmn-Hant-TW';//'zh-TW';//'en-US';'en-US';'cmn-Hant-TW';
-  //recognition.interimResults = false;
   recognition.maxAlternatives = 1;
 
 
   if (navigator.userAgent.indexOf("Chrome") != -1) {
-    var speechRecognitionList = new webkitSpeechGrammarList();
-    var grammar = '#JSGF V1.0; grammar volumes; public <volume> = ' + fullname.join(' | ') + ' ;';
-    speechRecognitionList.addFromString(grammar, 1.0);
-    recognition.grammars = speechRecognitionList;
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
+
+    const recognition = new SpeechRecognition();
+    const grammarList = new SpeechGrammarList();
+
+    //var grammar = '#JSGF V1.0; grammar volumes; public <volume> = ' + fullname.join(' | ') + ' ;';
+    const grammar = `
+#JSGF V1.0;
+grammar bible;
+
+public <book> = 創世記 | 出埃及記 | 利未記 | 民數記 | 申命記 |
+約書亞記 | 士師記 | 路得記 | 撒母耳記上 | 撒母耳記下 | 列王紀上 | 列王紀下 | 歷代志上 | 歷代志下 | 以斯拉記 | 尼希米記 | 以斯帖記 |
+約伯記 | 詩篇 | 箴言 | 傳道書 | 雅歌 | 
+以賽亞書 | 耶利米書 | 耶利米哀歌 | 以西結書 | 但以理書 | 
+何西阿書 | 約珥書 | 阿摩司書 | 俄巴底亞書 | 約拿書 | 彌迦書 | 那鴻書 | 哈巴谷書 | 西番雅書 | 哈該書 | 撒迦利亞書 | 瑪拉基書 | 
+馬太福音 | 馬可福音 | 路加福音 | 約翰福音 | 使徒行傳 | 
+羅馬書 | 哥林多前書 | 哥林多後書 | 加拉太書 | 以弗所書 | 腓立比書 | 歌羅西書 | 帖撒羅尼迦前書 | 帖撒羅尼迦後書 | 
+提摩太前書 | 提摩太後書 | 提多書 | 腓利門書 |   
+希伯來書 | 雅各書 | 彼得前書 | 彼得後書 |  
+約翰壹書 | 約翰貳書 | 約翰參書 | 
+猶大書 | 啟示錄 ;
+
+public <command> = <book> <number> 章 <number> 節 ;
+`;
+
+    //public <number> = 一 | 二 | 三 | 四 | 五 | 六 | 七 | 八 | 九 | 十 | 十一 | 十二 | 十三 | 二十 | 三十 ; 
+
+    grammarList.addFromString(grammar, 1);
+    recognition.grammars = grammarList;
+
+    //var speechRecognitionList = new webkitSpeechGrammarList();
+    //var grammar = '#JSGF V1.0; grammar volumes; public <volume> = ' + fullname.join(' | ') + ' ;';
+    //speechRecognitionList.addFromString(grammar, 1.0);
+    //recognition.grammars = speechRecognitionList;
   }
 
 
